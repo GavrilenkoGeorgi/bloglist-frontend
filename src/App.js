2165b83b286d2c06d0c0eb8155ac7cfe47b95640
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-12-23 17:00:38 +0200

    Add footer component and rename nav component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Notification from './components/Notification'
-import Navigation from './components/Navigation'
+import NavigationBar from './components/NavigationBar'
 import BlogAddForm from './components/BlogAddForm'
 import LoginForm from './components/LoginForm'
 import SignUp from './components/SignUp'
 import Togglable from './components/Togglable'
+import Footer from './components/Footer'
 import Blogs from './components/Blogs'
 import BlogPost from './components/BlogPost'
 import UsersList from './components/UsersList'
 import UserInfo from './components/UserInfo'
 import { setUserFromLocalStorage } from './reducers/userReducer'
 import './css/index.css'
+import { Container } from 'react-bootstrap'
 
 const App = (props) => {
 	const blogFormRef = React.createRef()
@@ -36,36 +38,41 @@ const App = (props) => {
 	}, [props, props.user, props.setUserFromLocalStorage])
 
 	return (
-		<div data-testid="main">
+		<>
 			<Router>
+			<NavigationBar />
+			<Container role="main" data-cy="mainContainer">
 				<Notification />
 				<h1>Blogs app.</h1>
-				{props.user ? (
-					<div>
-						<Navigation />
-						<Route path="/login" render={() => <LoginForm />} />
-						<Route exact path="/" render={() =>
-							<Togglable buttonLabel="new blog" dataCy="addBlogFormToggle" ref={blogFormRef}>
-								<BlogAddForm />
-							</Togglable>} />
-						<Route exact path="/users" render={() => <UsersList />} />
-						<Route exact path="/users/:id" render={({ match }) =>
-							<UserInfo userId={match.params.id} />
-						} />
-						<Route exact path="/blogs" render={() => <Blogs />} />
-						<Route exact path="/blogs/:id" render={({ match }) =>
-							<BlogPost blogId={match.params.id} />
-						} />
-					</div>
-				) :
-					<>
-						<LoginForm />
-							<Togglable buttonLabel="sign up" dataCy="signUp" ref={signUpRef}>
-							<SignUp />
-						</Togglable>
-					</>}
+				{props.user
+					? (
+						<div>
+							<Route path="/login" render={() => <LoginForm />} />
+							<Route exact path="/" render={() =>
+								<Togglable buttonLabel="new blog" dataCy="addBlogFormToggle" ref={blogFormRef}>
+									<BlogAddForm />
+								</Togglable>} />
+							<Route exact path="/users" render={() => <UsersList />} />
+							<Route exact path="/users/:id" render={({ match }) =>
+								<UserInfo userId={match.params.id} />
+							} />
+							<Route exact path="/blogs" render={() => <Blogs />} />
+							<Route exact path="/blogs/:id" render={({ match }) =>
+								<BlogPost blogId={match.params.id} />
+							} />
+						</div>
+					) :
+						<>
+							<LoginForm />
+								<Togglable buttonLabel="sign up" dataCy="signUp" ref={signUpRef}>
+								<SignUp />
+							</Togglable>
+						</>
+				}
+				</Container>
+				<Footer />
 			</Router>
-		</div>
+		</>
 	)
 }
 
