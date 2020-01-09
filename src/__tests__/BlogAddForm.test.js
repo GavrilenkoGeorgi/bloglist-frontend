1b569afe4fc404a46d108f8a339a84b1d7468524
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-01-09 15:03:29 +0200

    Fix blog add form tests

--- src/__tests__/BlogAddForm.test.js
+++ src/__tests__/BlogAddForm.test.js
@@ -1,43 +1,81 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
 import BlogAddForm from '../components/BlogAddForm'
 import { Provider } from 'react-redux'
 import { render, fireEvent, cleanup } from '@testing-library/react'
 import store from '../store'
 import App from '../App'
 import Notification from '../components/Notification'
-
-// import Context from '../../store/context'
+import user from '../__mocks__/testUser'
 
 afterEach(cleanup)
 
-it('Submiting add blog form works correctly', () => {
-	store.user = {
-		token: "ayJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhbmtAYW9sLmNvbSIsImlkIjoiNWRmY2RhNGU0M2IzYmQxZTRjMzllZDA2IiwiaWF0IjoxNTc4MjM2MjgwfQ.LpHQr-0vkvKHaC-sbJ99SZx2v8aeE3YlnSfqnOl9IVQ",
-		username: "Dale 'tester' Gribble",
-		email: "dale@aol.com",
-		id: "5dfcda4e43b3bd1e4c39ed07"
-	}
-	window.localStorage.setItem(
-		'loggedUserJSON', JSON.stringify(store.user)
-	)
-	const { getByTestId, getByText, debug } = render(
-		<Provider store={store}>
-			<App>
-				<Notification />
-				<BlogAddForm />
-			</App>
-		</Provider>
-	)
+describe('Blog add form', () => {
+	let wrapper
+	let titleInput
+	let authorInput
+	let urlInput
 
-	expect(getByText(/Title/i).textContent).toBe('Title')
-
-	// accessing form data from the synthetic event when
-	// user submits a form
-	// with text1 being the id of our input element
-	// and using data-testid property of the form to address it
-	fireEvent.submit(getByTestId('addBlogForm'), { target: { title: { value: 'Test title' } } })
-
-	// debug()
-	// expect(getByText(/Title/i).textContent).not.toBe('')
+	beforeEach(() => {
+		store.user = user
+		window.localStorage.setItem(
+			'loggedUserJSON', JSON.stringify(user)
+		)
+		wrapper = render(
+			<Provider store={store}>
+				<App>
+					<Notification />
+					<BlogAddForm />
+				</App>
+			</Provider>
+		)
+		titleInput = wrapper.getByLabelText('Title')
+		authorInput = wrapper.getByLabelText('Author')
+		urlInput = wrapper.getByLabelText('Url')
 	})
+
+	it('submits form data correctly', async () => {
+
+		fireEvent.change(titleInput, { target: { value: 'Test title' } })
+		expect(titleInput.value).toBe('Test title')
+
+		fireEvent.change(authorInput, { target: { value: 'Test author' } })
+		expect(authorInput.value).toBe('Test author')
+
+		fireEvent.change(urlInput, { target: { value: 'Test url' } })
+		expect(urlInput.value).toBe('Test url')
+
+		// accessing form data from the synthetic event when
+		// user submits a form
+		// with title, author and url being the ids of our input element
+		// and using data-testid property of the form to address it
+		// maybe use a button that is visible to the user?
+		fireEvent.submit(wrapper.getByTestId('addBlogForm'), {
+			target:	{
+					title: { value: 'Test title' },
+					author: { value: 'Test author' },
+					url: { value: 'Test url' }
+				}
+		})
+
+		// visible only after submmiting the form
+		const alert = await wrapper.findByRole('alert')
+		expect(alert.classList.contains('alert-success')).toBe(true)
+		expect(wrapper.container).toHaveTextContent('Blog successfully added.')
+	})
+
+	it('displays error if input data is missing', async () => {
+		fireEvent.submit(wrapper.getByTestId('addBlogForm'), {
+			target:	{
+					title: { value: '' },
+					author: { value: 'Test author' },
+					url: { value: 'Test url' }
+				}
+		})
+
+		const alert = await wrapper.findByRole('alert')
+		expect(alert.classList.contains('alert-danger')).toBe(true)
+		expect(wrapper.container).toHaveTextContent('Some fields are empty.')
+	})
+})
+
+
